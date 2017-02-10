405188e8759ac00bc61d75ddb6a4410fa98f8a9f
EyckWu <EyckWu@gmail.com>
2017-02-10 17:09:13 +0800

    增加点击欢迎界面立即进入主界面功能，解决创建多个MainActivity的bug

--- app/src/main/java/com/eyck/mobileplayer/LauncherActivity.java
+++ app/src/main/java/com/eyck/mobileplayer/LauncherActivity.java
@@ -5,17 +5,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class LauncherActivity extends Activity {
+
+    private static final String TAG = LauncherActivity.class.getSimpleName();
 
     private Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
+            startMainActivity();
+        }
+    };
+
+    //解决创建多个MainActivity bug
+    private boolean isFirstStart = true;
+    private void startMainActivity(){
+        Log.d(TAG,"LauncherActivity>startMainActivity>isFirstStart="+isFirstStart);
+        if(isFirstStart) {
+            isFirstStart = false;
             Intent intent = new Intent(LauncherActivity.this,MainActivity.class);
             startActivity(intent);
             finish();
         }
-    };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +43,12 @@ public class LauncherActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         handler.removeCallbacksAndMessages(null);//移除为处理的消息
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        startMainActivity();
+        return true;
     }
 }
