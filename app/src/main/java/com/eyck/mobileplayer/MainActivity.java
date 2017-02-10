b44c9f625ab34ebec7b562d9b0a0516cc7aa763b
EyckWu <EyckWu@gmail.com>
2017-02-10 19:01:22 +0800

    完成主界面布局

--- app/src/main/java/com/eyck/mobileplayer/MainActivity.java
+++ app/src/main/java/com/eyck/mobileplayer/MainActivity.java
@@ -2,16 +2,13 @@ package com.eyck.mobileplayer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        TextView textView = new TextView(this);
-        textView.setText("我是主界面");
-        setContentView(textView);
+        setContentView(R.layout.activity_main);
+
     }
 }
