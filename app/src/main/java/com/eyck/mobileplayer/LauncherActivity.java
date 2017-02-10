c542543454a3a035597b0ba562bb8ece92ff66be
EyckWu <EyckWu@gmail.com>
2017-02-10 16:36:43 +0800

    完成LauncherActivity(欢迎界面),3s后跳转到MainActivity(主界面)

--- app/src/main/java/com/eyck/mobileplayer/LauncherActivity.java
+++ app/src/main/java/com/eyck/mobileplayer/LauncherActivity.java
@@ -1,13 +1,33 @@
 package com.eyck.mobileplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 public class LauncherActivity extends Activity {
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Intent intent = new Intent(LauncherActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        handler.sendEmptyMessageDelayed(0,3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);//移除为处理的消息
     }
 }
