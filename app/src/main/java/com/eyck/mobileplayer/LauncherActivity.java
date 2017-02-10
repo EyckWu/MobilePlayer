058f03fc9701ef568d2fbb9611ecbe18fa1ca4ff
EyckWu <EyckWu@gmail.com>
2017-02-10 16:05:51 +0800

    项目创建

---
+++ app/src/main/java/com/eyck/mobileplayer/LauncherActivity.java
@@ -0,0 +1,13 @@
+package com.eyck.mobileplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
